950fbb64b0752494ec5254a8b1ff77e21957b810
TuanNx <tuan9864543@gmail.com>
2025-10-03 15:29:31 +0700

    small change to basic UI

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,38 +5,20 @@ import '../pages/search_page.dart';
 import '../pages/library_page.dart';
 import '../pages/premium_page.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    const HomePage(),
-    const SearchPage(),
-    const LibraryPage(),
-    const PremiumPage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
+      body: BottomNavBar(
+        pages: const [
+          HomePage(),
+          SearchPage(),
+          LibraryPage(),
+          PremiumPage(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
--- lib/widgets/navigation.dart
+++ lib/widgets/navigation.dart
@@ -1,46 +1,65 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
+class BottomNavBar extends StatefulWidget {
+  final List<Widget> pages;
 
   const BottomNavBar({
     Key? key,
-    required this.currentIndex,
-    required this.onTap,
+    required this.pages,
   }) : super(key: key);
 
   @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.black,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-      selectedFontSize: 12,
-      unselectedFontSize: 12,
-      elevation: 0,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
-          activeIcon: Icon(Icons.home),
-          label: 'Trang chủ',
+    return Column(
+      children: [
+        Expanded(
+          child: IndexedStack(
+            index: _selectedIndex,
+            children: widget.pages,
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Tìm kiếm',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.library_music_outlined),
-          activeIcon: Icon(Icons.library_music),
-          label: 'Thư viện',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.workspace_premium_outlined),
-          activeIcon: Icon(Icons.workspace_premium),
-          label: 'Premium',
+        BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Trang chủ',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Tìm kiếm',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_music_outlined),
+              activeIcon: Icon(Icons.library_music),
+              label: 'Thư viện',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.workspace_premium_outlined),
+              activeIcon: Icon(Icons.workspace_premium),
+              label: 'Premium',
+            ),
+          ],
         ),
       ],
     );
